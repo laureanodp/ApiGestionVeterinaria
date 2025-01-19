f6b77846f2572b2a7f1c56af6bbddf18972efaa1
de Priego Laureano <laureano.1998@gmail.com>
2025-01-18 21:02:02 -0300

    ultimo cambio para produccion

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 // server.js
+console.log("Aplicación iniciando...");
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -20,8 +22,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/clientes", clienteRoutes);
 app.use("/api/mascotas", mascotaRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+// Exportar el handler para Vercel
+module.exports = app;
